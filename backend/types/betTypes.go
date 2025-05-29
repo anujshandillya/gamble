4ec58d23612cf126dca4d368ed3e0f4215421738
Anuj Sharma <95935183+anujshandillya@users.noreply.github.com>
2025-05-29 23:32:37 +0530

    refactor: clean up

--- backend/types/betTypes.go
+++ backend/types/betTypes.go
@@ -41,8 +41,8 @@ type BetCoinToss struct {
 }
 
 type NextCoinToss struct {
-	Round     uint8
-	Selection uint8
+	Round     int
+	Selection int
 }
 
 // DragonTower
@@ -53,7 +53,8 @@ type BetDragonTower struct {
 }
 
 type NextDragonTower struct {
-	Egg int
+	Egg   int
+	Level int
 }
 
 // Mines
@@ -98,105 +99,60 @@ type NextBJ struct {
 }
 
 type ActiveBetCoinToss struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      [][]int `json:"state"`
 }
 type ActiveBetDragonTower struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	Difficulty string  `json:"difficulty"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      [][]int `json:"state"`
+	LevelSet   [][]int `json:"levelSet"`
 }
 type ActiveBetMines struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	MinesCount uint8   `json:"minesCount"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      []uint8 `json:"state"`
+	MinesSet   []uint8 `json:"minesSet"`
 }
 type ActiveBetBJ struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      [][]int `json:"state"`
 }
 type ActiveBetPump struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      [][]int `json:"state"`
 }
 type ActiveBetHighLow struct {
-	Game       string    `json:"game"`
-	ServerSeed string    `json:"serverSeed"`
-	ClientSeed string    `json:"clientSeed"`
-	Nonce      uint16    `json:"nonce"`
-	Status     string    `json:"status"`
-	Amount     float64   `json:"amount"`
-	State      [][]uint8 `json:"state"`
+	Game       string  `json:"game"`
+	ServerSeed string  `json:"serverSeed"`
+	ClientSeed string  `json:"clientSeed"`
+	Nonce      uint16  `json:"nonce"`
+	Status     string  `json:"status"`
+	Amount     float64 `json:"amount"`
+	State      [][]int `json:"state"`
 }
-
-// Common methods for all active bet types
-func (b ActiveBetCoinToss) GetGame() string       { return b.Game }
-func (b ActiveBetCoinToss) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetCoinToss) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetCoinToss) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetCoinToss) GetStatus() string     { return b.Status }
-func (b ActiveBetCoinToss) GetAmount() float64    { return b.Amount }
-func (b ActiveBetCoinToss) GetState() [][]uint8   { return b.State }
-
-func (b ActiveBetDragonTower) GetGame() string       { return b.Game }
-func (b ActiveBetDragonTower) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetDragonTower) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetDragonTower) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetDragonTower) GetStatus() string     { return b.Status }
-func (b ActiveBetDragonTower) GetAmount() float64    { return b.Amount }
-func (b ActiveBetDragonTower) GetState() [][]uint8   { return b.State }
-
-func (b ActiveBetMines) GetGame() string       { return b.Game }
-func (b ActiveBetMines) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetMines) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetMines) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetMines) GetStatus() string     { return b.Status }
-func (b ActiveBetMines) GetAmount() float64    { return b.Amount }
-func (b ActiveBetMines) GetState() [][]uint8   { return b.State }
-
-func (b ActiveBetBJ) GetGame() string       { return b.Game }
-func (b ActiveBetBJ) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetBJ) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetBJ) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetBJ) GetStatus() string     { return b.Status }
-func (b ActiveBetBJ) GetAmount() float64    { return b.Amount }
-func (b ActiveBetBJ) GetState() [][]uint8   { return b.State }
-
-func (b ActiveBetPump) GetGame() string       { return b.Game }
-func (b ActiveBetPump) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetPump) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetPump) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetPump) GetStatus() string     { return b.Status }
-func (b ActiveBetPump) GetAmount() float64    { return b.Amount }
-func (b ActiveBetPump) GetState() [][]uint8   { return b.State }
-
-func (b ActiveBetHighLow) GetGame() string       { return b.Game }
-func (b ActiveBetHighLow) GetServerSeed() string { return b.ServerSeed }
-func (b ActiveBetHighLow) GetClientSeed() string { return b.ClientSeed }
-func (b ActiveBetHighLow) GetNonce() uint16      { return b.Nonce }
-func (b ActiveBetHighLow) GetStatus() string     { return b.Status }
-func (b ActiveBetHighLow) GetAmount() float64    { return b.Amount }
-func (b ActiveBetHighLow) GetState() [][]uint8   { return b.State }
